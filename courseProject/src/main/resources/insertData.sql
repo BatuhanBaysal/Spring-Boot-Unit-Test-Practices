d59f76be7e424fd57705da0a98d73573f900beba
Batuhan Baysal <batuhanbaysal3@gmail.com>
2024-11-22 17:12:27 +0300

    Create Resources

---
+++ courseProject/src/main/resources/insertData.sql
@@ -0,0 +1,5 @@
+insert into student(id, firstname, lastname, email_address) values (11, 'Ali', 'Veli', 'ali.veli@gmail.com')
+insert into student(id, firstname, lastname, email_address) values (12, 'Batu', 'Bay', 'batu.bay@gmail.com')
+insert into student(id, firstname, lastname, email_address) values (13, 'Semih', 'Tek', 'semih.tek@gmail.com')
+insert into student(id, firstname, lastname, email_address) values (14, 'Pinar', 'Veli', 'pinar.veli@gmail.com')
+insert into student(id, firstname, lastname, email_address) values (15, 'Hasan', 'Veli', 'hasan.veli@gmail.com')
