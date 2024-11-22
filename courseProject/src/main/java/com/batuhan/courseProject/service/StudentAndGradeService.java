dce252400ec9b00b484ba4bb3627bf8d55bf02cd
Batuhan Baysal <batuhanbaysal3@gmail.com>
2024-11-22 17:11:20 +0300

    Create StudentAndGradeService

---
+++ courseProject/src/main/java/com/batuhan/courseProject/service/StudentAndGradeService.java
@@ -0,0 +1,172 @@
+package com.batuhan.courseProject.service;
+
+import com.batuhan.courseProject.models.*;
+import com.batuhan.courseProject.repository.HistoryGradesDao;
+import com.batuhan.courseProject.repository.MathGradesDao;
+import com.batuhan.courseProject.repository.ScienceGradesDao;
+import com.batuhan.courseProject.repository.StudentDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class StudentAndGradeService {
+
+    @Autowired
+    private StudentDao studentDao;
+
+    @Autowired
+    private MathGradesDao mathGradesDao;
+
+    @Autowired
+    @Qualifier("mathGrades")
+    private MathGrade mathGrade;
+
+    @Autowired
+    ScienceGradesDao scienceGradesDao;
+
+    @Autowired
+    @Qualifier("scienceGrades")
+    private ScienceGrade scienceGrade;
+
+    @Autowired
+    HistoryGradesDao historyGradesDao;
+
+    @Autowired
+    @Qualifier("historyGrades")
+    private HistoryGrade historyGrade;
+
+    @Autowired
+    private StudentGrades studentGrades;
+
+    public void createStudent(String firstname, String lastname, String emailAddress) {
+        CollegeStudent student = new CollegeStudent(firstname, lastname, emailAddress);
+        student.setId(1);
+        studentDao.save(student);
+    }
+
+    public boolean checkIfStudentIsNull(int id) {
+        Optional<CollegeStudent> student = studentDao.findById(id);
+        if (student.isPresent()) {
+            return true;
+        }
+        return false;
+    }
+
+    public void deleteStudent(int id) {
+        if (checkIfStudentIsNull(id)) {
+            studentDao.deleteById(id);
+            mathGradesDao.deleteByStudentId(id);
+            scienceGradesDao.deleteByStudentId(id);
+            historyGradesDao.deleteByStudentId(id);
+        }
+    }
+
+    public  Iterable<CollegeStudent> getGradebook() {
+        Iterable<CollegeStudent> collegeStudents = studentDao.findAll();
+        return collegeStudents;
+    }
+
+    public boolean createGrade(double grade, int studentId, String gradeType) {
+
+        if (!checkIfStudentIsNull(studentId)) {
+            return false;
+        }
+
+        if (grade >= 0 && grade <= 100) {
+            if (gradeType.equals("math")) {
+                mathGrade.setId(1);
+                mathGrade.setGrade(grade);
+                mathGrade.setStudentId(studentId);
+                mathGradesDao.save(mathGrade);
+                return true;
+            }
+            if (gradeType.equals("science")) {
+                scienceGrade.setId(1);
+                scienceGrade.setGrade(grade);
+                scienceGrade.setStudentId(studentId);
+                scienceGradesDao.save(scienceGrade);
+                return true;
+            }
+            if (gradeType.equals("history")) {
+                historyGrade.setId(1);
+                historyGrade.setGrade(grade);
+                historyGrade.setStudentId(studentId);
+                historyGradesDao.save(historyGrade);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int deleteGrade(int id, String gradeType) {
+        int studentId = 0;
+        if (gradeType.equals("math")) {
+            Optional<MathGrade> grade = mathGradesDao.findById(id);
+            if (!grade.isPresent()) {
+                return studentId;
+            }
+
+            studentId = grade.get().getStudentId();
+            mathGradesDao.deleteById(id);
+        }
+
+        if (gradeType.equals("science")) {
+            Optional<ScienceGrade> grade = scienceGradesDao.findById(id);
+            if (!grade.isPresent()) {
+                return studentId;
+            }
+
+            studentId = grade.get().getStudentId();
+            scienceGradesDao.deleteById(id);
+        }
+
+        if (gradeType.equals("history")) {
+            Optional<HistoryGrade> grade = historyGradesDao.findById(id);
+            if (!grade.isPresent()) {
+                return studentId;
+            }
+
+            studentId = grade.get().getStudentId();
+            historyGradesDao.deleteById(id);
+        }
+
+        return studentId;
+    }
+
+    public GradebookCollegeStudent studentInformation(int id) {
+        if (!checkIfStudentIsNull(id)) {
+            return null;
+        }
+
+        Optional<CollegeStudent> student = studentDao.findById(id);
+        Iterable<MathGrade> mathGrades = mathGradesDao.findGradeByStudentId(id);
+        Iterable<ScienceGrade> scienceGrades = scienceGradesDao.findGradeByStudentId(id);
+        Iterable<HistoryGrade> historyGrades = historyGradesDao.findGradeByStudentId(id);
+
+        List<Grade> mathGradesList = new ArrayList<>();
+        mathGrades.forEach(mathGradesList::add);
+
+        List<Grade> scienceGradesList = new ArrayList<>();
+        scienceGrades.forEach(scienceGradesList::add);
+
+        List<Grade> historyGradesList = new ArrayList<>();
+        historyGrades.forEach(historyGradesList::add);
+
+        studentGrades.setMathGradeResults(mathGradesList);
+        studentGrades.setScienceGradeResults(scienceGradesList);
+        studentGrades.setHistoryGradeResults(historyGradesList);
+
+        GradebookCollegeStudent gradebookCollegeStudent = new GradebookCollegeStudent(student.get().getId(),
+                student.get().getFirstname(), student.get().getLastname(), student.get().getEmailAddress(),
+                studentGrades);
+
+        return gradebookCollegeStudent;
+    }
+}
