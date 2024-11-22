e9fbb7ea29397ee253be89c4db078272b2c91779
Batuhan Baysal <batuhanbaysal3@gmail.com>
2024-11-22 17:05:54 +0300

    Create Course Project

---
+++ courseProject/src/main/java/com/batuhan/courseProject/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.batuhan.courseProject;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MvcTestingExampleApplication.class);
+	}
+}
